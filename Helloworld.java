bf64f9e0cd2b376fa4a5b5a279e2a24b4fd717d3
krishnakaruchola <krishnakanthkaruchola@gmail.com>
2024-03-06 08:24:32 +0530

    i have added two files

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+package com.careerit.jfs.day1;
+
+public class Helloworld {
+    public static void main(String []args){
+        System.out.println("Welcome to Day 1 Sesion of Java Full Stack Developer Course");
+    System.out.println("Welcome To Java Programming Learning");
+    }
+}
---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.careerit.jfs.day1;
+
+public class SumOfTwoNumbers {
+    public static void main(String[]args){
+        int number1=1010,number2=1090;
+        int sum;
+        sum=number1+number2;
+        System.out.println("Sum of two numbers num1 "+number1+" and num2 "+number2+" = "+sum);
+
+    }
+}
