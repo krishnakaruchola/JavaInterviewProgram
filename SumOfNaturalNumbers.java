c0b242ded901c727dd93ac5cd68fb8bbd1f2fe6c
krishnakaruchola <krishnakanthkaruchola@gmail.com>
2024-03-06 16:45:36 +0530

    sum of n natural numbers program

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,13 @@
+package com.careerit.jfs.day1;
+
+public class SumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+
+        int num=10;
+        int sum=0;
+        sum=num*(num+1)/2;
+        System.out.println("The Sum of first "+num +" natural numbers is "+sum);
+
+    }
+}
