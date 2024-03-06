c6397903418ad7996f93c09f946e20a6fa099dfc
krishnakaruchola <krishnakanthkaruchola@gmail.com>
2024-03-06 17:05:37 +0530

    muliplication table

---
+++ MulitiplicationOfTable.java
@@ -0,0 +1,12 @@
+package com.careerit.jfs.day1;
+
+public class MulitiplicationOfTable {
+    public static void main(String []args)
+    {
+        int num =5;
+        for(int i=1;i<=10;i++)
+        {
+          System.out.println(num+ " * "+ i+" = " +(num*i));
+        }
+    }
+}
